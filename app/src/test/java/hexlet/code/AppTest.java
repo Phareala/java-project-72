5fc7481dd1a6804cf3fbd640f0daef811b17ac7d
paul <pashaosadchii@mail.ru>
2023-11-22 15:39:37 +0300

    Setup Test

---
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -0,0 +1,18 @@
+package hexlet.code;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+    @Test
+    void test () {
+        assertThat(1).isEqualTo(1);
+    }
+}
